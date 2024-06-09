46ca82426aba9640e7dabcadf3a741487044a9d4
Hikari Kobe <h.kobe712@gmail.com>
2024-06-09 16:49:26 +0900

    fix: setup parser

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: { 'react/jsx-uses-react': 'off', 'react/react-in-jsx-scope': 'off' },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 7,
     sourceType: 'module',
