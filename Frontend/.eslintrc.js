de0ce7dd15990aaa40f2235d5e22e33f879cf25d
Hikari Kobe <h.kobe712@gmail.com>
2024-06-09 16:47:28 +0900

    update: modified config

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   rules: { 'react/jsx-uses-react': 'off', 'react/react-in-jsx-scope': 'off' },
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
 };
