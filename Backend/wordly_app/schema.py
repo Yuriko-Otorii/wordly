3024a5c13ee9a499449671a3f740e1204ff1cdc4
Yuri <104223310+Yuriko-Otorii@users.noreply.github.com>
2024-07-26 22:40:35 -0700

    Update: Add word update and delete mutation

--- Backend/wordly_app/schema.py
+++ Backend/wordly_app/schema.py
@@ -1,8 +1,8 @@
-from graphene import ObjectType, List, Field, ID
+from graphene import ObjectType, List, Field, ID, Int
 import graphql_jwt
 from django.contrib.auth import get_user_model
 from .authentication import CreateUser, LoginUser
-from .word_mutation import CreateWord
+from .word_mutation import CreateCategory, CreateWord, UpdateWord, DeleteWord
 from .models import Word, Category, Definition
 from .types import UserType, WordType, CategoryType, DefinitionType
 
@@ -12,11 +12,27 @@ class Query(ObjectType):
         return get_user_model().objects.all()
     
     words = List(WordType)
+    getWordsByUserId = List(WordType, user_id=ID())
+    getWordsByCategory = List(WordType, category_id=ID())
+    getWordsByMemoryProcess = List(WordType, memory_process=Int())
     categories = List(CategoryType, user_id=ID())
     definition = Field(DefinitionType, word_id=ID())
 
     def resolve_words(self, info):
         return Word.objects.all()
+    
+    def resolve_getWordsByUserId(self, info, user_id):
+        user = info.context.user
+        if user_id:
+            return Word.objects.filter(user_id=user_id)
+        return Word.objects.filter(user=user)
+    
+    def resolve_getWordsByCategory(self, info, category_id):
+        return Word.objects.filter(category_id=category_id)
+    
+    def resolve_getWordsByMemoryProcess(self, info, memory_process):
+        # ここでのreturnを、数を絞って複数のグループとして渡すか、一括で渡すか要検討。
+        return Word.objects.filter(memory_process=memory_process)
     
     def resolve_categories(self, info, user_id):
         user = info.context.user
@@ -30,6 +46,8 @@ class Query(ObjectType):
 class Mutation(ObjectType):
     signup = CreateUser.Field()
     login = LoginUser.Field()
-    
 
+    create_category = CreateCategory.Field()
     create_word = CreateWord.Field()
+    update_word = UpdateWord.Field()
+    delete_word = DeleteWord.Field()
--- Backend/wordly_app/word_mutation.py
+++ Backend/wordly_app/word_mutation.py
@@ -1,9 +1,31 @@
-from graphene import Mutation, Field, String, List
+from graphene import Mutation, Field, String, List, ID
 from graphql import GraphQLError
 from graphene_file_upload.scalars import Upload
-from .types import WordType
+from .types import WordType, CategoryType
 from .models import Word, Definition, Category, User
 
+
+class CreateCategory(Mutation):
+    class Arguments:
+        category_name = String(required=True)
+
+    category = Field(CategoryType)
+
+    def mutate(self, info, category_name):
+        try:
+            user = info.context.user
+            if not user.is_authenticated:
+                raise GraphQLError('Authentication required.')
+
+            category = Category.objects.create(
+                name = category_name,
+                user = user
+            )
+
+            return CreateCategory(category=category)
+        except Exception as e:
+            raise GraphQLError(str(e))
+    
 class CreateWord(Mutation):
     class Arguments:
         word = String(required=True)
@@ -16,28 +38,100 @@ class CreateWord(Mutation):
 
     word = Field(WordType)
 
-    def mutate(self, info, word, category_name, definition, example=[], pronunciation='', parts_of_speech='', image=None):
-        user = info.context.user
-        if not user.is_authenticated:
-            raise GraphQLError('Authentication required.')
+    def mutate(self, info, word, category_name, definition, example=None, pronunciation=None, parts_of_speech=None, image=None):
+        try:
+            user = info.context.user
+            if not user.is_authenticated:
+                raise GraphQLError('Authentication required.')
+            
+            category = Category.objects.filter(user=user, name=category_name).first()
 
-        category = Category.objects.filter(name=category_name, user=user).first()
-        if not category:
-            category = Category.objects.create(name=category_name, user=user)
+            new_word = Word.objects.create(
+                word = word,
+                category = category,
+                user = user,
+                pronunciation = pronunciation,
+                parts_of_speech = parts_of_speech,
+                example = example,
+                image = image,
+            )
 
-        new_word = Word.objects.create(
-            word = word,
-            category = category,
-            user = user,
-            pronunciation = pronunciation,
-            parts_of_speech = parts_of_speech,
-            example = example,
-            image = image,
-        )
+            Definition.objects.create(
+                word = new_word,
+                definition = definition
+            )
 
-        Definition.objects.create(
-            word = new_word,
-            definition = definition
-        )
+            return CreateWord(word=new_word)
+        except Exception as e:
+            raise GraphQLError(str(e))
 
-        return CreateWord(word=new_word)
+class UpdateWord(Mutation):
+    class Arguments:
+        word_id = ID(required=True)
+        word = String()
+        category_name = String()
+        definition = List(String)
+        example = List(String)
+        pronunciation = String()
+        parts_of_speech = String()
+        image = Upload()
+
+    word = Field(WordType)
+
+    def mutate(self, info, word_id, word=None, category_name=None, definition=None, example=None, pronunciation=None, parts_of_speech=None, image=None):
+        try:
+            user = info.context.user
+            if not user.is_authenticated:
+                raise GraphQLError('Authentication required.')
+            
+            category = Category.objects.filter(user=user, name=category_name).first()
+
+            target_word = Word.objects.filter(user=user, id=word_id).first()
+            if target_word is None:
+                raise GraphQLError('Word not found.')
+            
+            if word is not None:
+                target_word.word = word
+            if category is not None:
+                target_word.category = category
+            if example is not None:
+                target_word.example = example
+            if pronunciation is not None:
+                target_word.pronunciation = pronunciation
+            if parts_of_speech is not None:
+                target_word.parts_of_speech = parts_of_speech
+            if image is not None:
+                target_word.image = image
+
+            target_word.save()
+
+            if definition is not None:
+                definition = Definition.objects.filter(word=word).first()
+                definition.definition = definition
+                definition.save()
+
+            return UpdateWord(word=target_word)
+        except Exception as e:
+            raise GraphQLError(str(e))
+
+class DeleteWord(Mutation):
+    class Arguments:
+        word_id = ID(required=True)
+
+    word = Field(WordType)
+
+    def mutate(self, info, word_id):
+        try:
+            user = info.context.user
+            if not user.is_authenticated:
+                raise GraphQLError('Authentication required.')
+
+            word = Word.objects.filter(user=user, id=word_id).first()
+            if word is None:
+                raise GraphQLError('Word not found.')
+
+            word.delete()
+
+            return DeleteWord(word=word)
+        except Exception as e:
+            raise GraphQLError(str(e))
