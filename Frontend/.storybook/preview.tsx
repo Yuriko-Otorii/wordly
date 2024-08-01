66ef94db47f1fb4ba4559ab6b56353a8f82dc0a6
Hikari Kobe <h.kobe712@gmail.com>
2024-08-01 23:21:50 +0900

    setup: radix-theme

--- Frontend/.storybook/preview.tsx
+++ Frontend/.storybook/preview.tsx
@@ -1,5 +1,7 @@
+import { Theme } from '@radix-ui/themes';
 import type { Preview } from '@storybook/react';
 import '../src/app/globals.css';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +12,15 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => {
+      return (
+        <Theme>
+          <Story />
+        </Theme>
+      );
+    },
+  ],
 };
 
 export default preview;
