1a0bf0364a799e34f7740224d2a2d71e597a2fc8
Hikari Kobe <h.kobe712@gmail.com>
2024-08-01 23:23:47 +0900

    update: utilized Theme and changed fonts

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header';
+import { Header } from '@/features/components/layout/Header';
 
 export default function Home() {
   return (
--- Frontend/src/features/components/layout/Header/index.tsx
+++ Frontend/src/features/components/layout/Header/index.tsx
@@ -1,3 +1,12 @@
+import { Button, Flex, Text } from '@radix-ui/themes';
+
 export const Header = () => {
-  return <div>index</div>;
+  return (
+    <Flex direction='column' gap='2'>
+      <Text>Hello from Radix Themes :)</Text>
+      <Button size='1' variant='soft'>
+        Let's go
+      </Button>
+    </Flex>
+  );
 };
