62b1d2f0f886fc8642613a2b50832840fbeb6c2f
Hikari Kobe <h.kobe712@gmail.com>
2024-08-30 15:43:14 +0900

    update: added label

--- Frontend/src/components/ui/Common/Input/index.stories.tsx
+++ Frontend/src/components/ui/Common/Input/index.stories.tsx
@@ -6,6 +6,7 @@ const meta: Meta<typeof Input> = {
   args: {
     maxWidth: '240px',
     placeholder: 'type something...',
+    label: 'Word',
   },
 };
 
--- Frontend/src/components/ui/Common/Input/index.tsx
+++ Frontend/src/components/ui/Common/Input/index.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import { Box, TextField } from '@radix-ui/themes';
+import { Box, Text, TextField } from '@radix-ui/themes';
 import { Responsive } from '@radix-ui/themes/props';
 
 type Props = {
   maxWidth: string;
   size: Responsive<'1' | '2' | '3'>;
   placeholder: string;
+  label: string;
 };
 
 export const Input: React.FC<Props> = (props) => {
   return (
     <Box maxWidth={props.maxWidth}>
+      <Text size='2' as='label'>
+        {props.label}
+      </Text>
       <TextField.Root size={props.size} placeholder={props.placeholder} />
     </Box>
   );
